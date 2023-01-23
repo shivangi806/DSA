223cffffae6fb7d141471b4128cf962a2cc3a443
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-01-23 21:02:13 +0530

    Time: 905 ms (5.02%), Space: 12.2 MB (24.42%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -4,16 +4,19 @@ public:
         int n = strs.size();
         int m = strs[0].size();
         int ans=0;
-        int i=1;
         for(int j=0;j<m;j++){
             int c=0;
-            for(int i=1;i<n;i++){
-                if( strs[i-1][j]<=strs[i][j]){
+            int i=1;
+            while(i<n){
+                if(strs[i-1][j]<=strs[i][j]){
                     c++;
+                    cout<<'i'<<i;
                 }   
+                i++;
             }
             if(c!=(n-1)) {
                 ans++;
+                cout<<j;
             }
         }
         return ans ;
