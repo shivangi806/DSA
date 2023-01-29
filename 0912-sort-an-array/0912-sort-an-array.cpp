b246b56c5d97a2de792e9f2c614b6f3628121093
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-01-30 00:52:38 +0530

    Time: 259 ms (54.42%), Space: 90.8 MB (35.32%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+     multiset<int> st;
+     for(int i=0;i<nums.size();i++){
+         st.insert(nums[i]);
+     }
+    vector<int> v;
+    for(auto x : st){
+        v.push_back(x);
+    }
+    return v;
+    }
+};
