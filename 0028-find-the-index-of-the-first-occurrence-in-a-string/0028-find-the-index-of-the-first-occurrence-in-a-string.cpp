9801aae93ce2f9a244fda287a623420d04bb065a
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-03-04 00:54:52 +0530

    Time: 4 ms (38.84%), Space: 6.2 MB (95.03%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,26 +1,12 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        // next method
-        // by first finding the first character of needle in haystack and then check the substring is equal or not if not then move forward to chcek another i position in haystack where we found the first character of the needle
-        
         int n= haystack.size();
         int m = needle.size();
-        int ans =-1;
-        int i=0;
-        while(i<n){
-            // find first character of needle in haystack
-            auto itr = find(haystack.begin()+i,haystack.end(),needle[0]);
-            // now substring check karenge
-            int pos = itr-haystack.begin();
-            string str = haystack.substr(pos , m);
-            if(str==needle) {
-                ans = pos;
-                break;
-            }else{
-                i++;
-            }
-        }
-        return ans;
+        // for finding the target string in given string and storing the first starting index of it in the element  ans
+        int ans = haystack.find(needle);
+        // after that checking ki vo subtring h ya ni 
+        if(ans!= string :: npos) return ans;
+        return -1;
     }
 };
