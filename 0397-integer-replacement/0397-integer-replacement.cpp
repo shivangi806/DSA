3e35845381ab16f4cde2f12d6ebddefe01d54ddc
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-03-09 23:27:07 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (14.33%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,11 +1,12 @@
 #define ll long long 
 class Solution {
 public:
-    
+    map<int,int> mp ;
     ll solve(ll i){
         if(i==1) return  0;
-        if(i%2==0) return 1 + solve(i/2) ;
-        return 1 + min(solve(i-1) , solve(i+1));
+        if(mp[i]!=0) return mp[i] ;
+        if(i%2==0) return  mp[i] =  1 + solve(i/2) ;
+        return mp[i] = 1 + min(solve(i-1) , solve(i+1));
     }
     int integerReplacement(int n) {
         return solve(n);
