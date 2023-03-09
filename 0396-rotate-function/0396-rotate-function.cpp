684f4949645b6d18f79fa5720b7b4d971a44031b
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-03-10 00:03:15 +0530

    Time: 155 ms (69.42%), Space: 95.9 MB (22.96%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.cpp
+++ 0396-rotate-function/0396-rotate-function.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int maxRotateFunction(vector<int>& nums) {
-        int sum =0;
-        int ans = 0;
         int n = nums.size();
+        int  k = n-1 ; // number of times an array will be rotated
+        int sum = 0;
+        int ans = 0;
         for(int i=0;i<n;i++){
-            sum+=nums[i] ;
+            sum+=nums[i];
             ans += i*nums[i];
         }
-        int res=ans ;
-        int idx = n-1;
-        for(int k=1 ;k <n  ;k++){
-            ans = ans   +  sum - nums[idx]*(n-1) - nums[idx] ;
-            res = max(res , ans) ;
-            idx--;
+        int comefirstindex = n-1 ;
+        int ret = ans ;
+        for(int i=0;i<k;i++){
+            ans = ans + ( sum - (n-1)*nums[comefirstindex] - nums[comefirstindex]);
+            ret = max(ret , ans);
+            comefirstindex--;
         }
-        return res;
+        return ret ;
     }
 };
