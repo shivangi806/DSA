8d98ebf0bb3c5e24fed0035cfbcdf3350498c771
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-03-17 01:34:39 +0530

    Time: 71 ms (18.58%), Space: 24.4 MB (13.75%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.size(),m=text2.size();
-        int t[n+1][m+1];
-        // initialisation for top down
+    int longestCommonSubsequence(string s, string t) {
+        int n = s.size() , m = t.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0)) ;
+        
         for(int i=0;i<n+1;i++){
             for(int j=0;j<m+1;j++){
-                if(i==0 || j==0) t[i][j]=0;
+                if(i==0 || j==0) {
+                    dp[i][j]=0;
+                }
             }
         }
-        // choice diagram in matrix format
-        // text1 -- in top to bootom
-        // text2 -- in left to right
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                // if matching
-                if(text1[i-1]==text2[j-1]){
-                    t[i][j] = 1+t[i-1][j-1];
+        
+        for(int i=1;i<=n;i++){
+            for(int  j =1 ;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] =1 + dp[i-1][j-1];
                 }
-                // if not matching
                 else{
-                   // we have two choices
-                    t[i][j] = max(t[i-1][j]  , t[i][j-1]);
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
                 }
             }
         }
-        return t[n][m] ;
+        return dp[n][m] ;
     }
 };
