0afa1843f7915cdfabab5655a6a61be6deec278f
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-05-04 12:35:47 +0530

    Added solution - LeetHub

--- Subset Sums - GFG/subset-sums.cpp
+++ Subset Sums - GFG/subset-sums.cpp
@@ -7,27 +7,37 @@ class Solution
 {
 public:
 
-    void solve(int i, vector<int>&arr,int N , int sum,vector<int>&ans){
-        if(i>=N){
-            ans.push_back(sum);
+    void solve(vector<int>&A,vector<int>&ds,int i,int sum){
+        if(i>=A.size()){
+            ds.push_back(sum); // at max ans me n elements honge so o(n)
             return ;
         }
+        // for generating all subsets calling function 2 times for each element
+        // O(2^n)
+        // take 
+        // ds.push_back(A[i]);
+        sum+=A[i];
+        solve(A,ds,i+1,sum);
+        sum-=A[i];
+        // ds.pop_back();
         
-        // take
-        solve(i+1,arr,N,sum+arr[i],ans);
-        
-        // no take
-        solve(i+1,arr,N,sum,ans);
+        // notake
+        solve(A,ds,i+1,sum);
     }
     vector<int> subsetSums(vector<int> arr, int N)
     {
         // Write Your Code here
-        // i have to collect all the subsets sum
-        // that is may be the element will be in our sum or it may not
-        int sum = 0;
-        vector<int> ans;
-        solve(0,arr,N,sum,ans);
-        return ans;
+        // first sol : generate all subsets formed
+        // collect it in temp vector
+        // collect all those temp in vector of vector ans 
+        // then sum all temp by iterating to all temp in ans 
+        // vector<vector<int>> ans ;
+        vector<int> ds;
+        int sum =0;
+        solve(arr , ds, 0,sum);
+        // sorting takes O(nlogn) ;
+        // sort(ans.begin(),ans.end());
+        return ds ;
     }
 };
 
