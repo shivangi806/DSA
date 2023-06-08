ae45b41a95558ad0dca42009f791c9099bb983e6
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-08 09:46:30 +0530

    Time: 52 ms (28.11%), Space: 36.8 MB (76.15%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -9,32 +9,56 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class Bstit{
+  stack<TreeNode*> st;
+  bool reverse=true;
+  public : 
+    Bstit(TreeNode* root , bool isreverse)
+    {
+        reverse=isreverse;
+        pushNode(root);
+    }
+    bool hasNext()
+    {
+        if(st.empty()) return 0;
+        return 1;
+    }
+    int next(){
+        TreeNode* root = st.top();
+        st.pop();
+        if(reverse)pushNode(root->left); // reverse h mtlb left h uska
+        else pushNode(root->right);
+        return root->val;
+    }
+    
+    private:
+    void pushNode(TreeNode* root)
+    {
+        while(root!=NULL)
+        {
+            st.push(root);
+            if(!reverse) root=root->left;
+            else root=root->right;
+        }
+    }
+};
 class Solution {
 public:
-    void inorder(vector<int>&in , TreeNode* root)
-    {
-        if(root==NULL) return;
-        inorder(in,root->left);
-        in.push_back(root->val);
-        inorder(in,root->right);
-    }
     bool findTarget(TreeNode* root, int k) {
-        vector<int> in;
-        inorder(in,root);
-        int i=0,j=in.size()-1;
+        Bstit lf(root,false); // next
+        Bstit rf(root,true); // before
+        int i = lf.next();
+        int j=rf.next();
         while(i<j)
         {
-            if(in[i]+in[j]==k)
+            if(i+j==k) return 1;
+            if(i+j < k)
             {
-                return 1;
-            }
-            else if(in[i]+in[j] > k)
-            {
-                j--;
+                i=lf.next();
             }
             else
             {
-                i++;
+                j=rf.next();
             }
         }
         return 0;
