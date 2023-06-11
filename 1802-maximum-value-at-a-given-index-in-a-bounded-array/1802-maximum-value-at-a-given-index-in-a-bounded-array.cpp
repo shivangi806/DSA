49f25878b93d5991ca3d00d6f9c9f4bf67f1f507
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-11 13:41:44 +0530

    Time: 5 ms (22.39%), Space: 6 MB (46.27%) - LeetHub

--- 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
+++ 1802-maximum-value-at-a-given-index-in-a-bounded-array/1802-maximum-value-at-a-given-index-in-a-bounded-array.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
-    int maxValue(int n, int index, int maxSum) {
-        maxSum -= n;
-        int left = 0, right = maxSum;
-        while (left < right) {
-            int mid = (left + right + 1) / 2;
-            if (isPossible(mid, index, n, maxSum)) {
-                left = mid;
-            } else {
-                right = mid - 1;
-            }
-        }
-        return left + 1;
-    }
+    #define ll long long
     
-    bool isPossible(int a, int index, int n, int sum) {
-        long long left = std::max(a - index, 0);
-        long long result = (a + left) * (a - left + 1) / 2;
-        long long right = std::max(a - ((n - 1) - index), 0);
-        result += (a + right) * (a - right + 1) / 2;
-        return (result - a <= sum);
+    ll sum(ll c , ll x){
+        return c*x - (c*(c+1))/2;
+    }
+    ll ans = 0;
+    int maxValue(int n, int index, int maxSum) {
+        ll l = 1,h=maxSum;
+        while(l<=h){
+            ll mid = (l+h)>>1;
+            
+            ll lc = min((ll)(index),mid-1);
+            ll rc= min((ll)(n-index-1) , mid-1);
+            
+            ll leftsum = sum(lc,mid);
+            leftsum+=max((ll)0,(ll)(index-(mid-1)));
+            
+            
+            ll rightsum = sum(rc,mid);
+            rightsum+=max((ll)0,(ll)(n-index-1-(mid-1)));
+            
+            ll ttsum = leftsum+rightsum+mid;
+            if(ttsum<=maxSum){
+                ans = max(ans ,mid);
+                l=mid+1;
+            }
+            else h=mid-1;
+        }
+        return  ans ;
     }
 };
