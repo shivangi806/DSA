0b1ed55d4f6ad5015c61d28e87016bcbbee0dc02
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-14 19:04:11 +0530

    Time: 131 ms (41.87%), Space: 49.7 MB (10.95%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,37 +1,26 @@
 class Solution {
 public:
+    vector<vector<int>> dp;
+    int solve(int i ,int j, vector<int>&coins ,int n){
+        if(j==0) return 0;
+        if(i==n-1){
+            if(j%coins[i]==0){
+                return j/coins[i];
+            }
+            return 1e9;
+        }
+        if(dp[i][j]!=-1) return dp[i][j] ;
+        int notake = solve(i+1 ,j,coins,n);
+        int take = INT_MAX;
+        if(j-coins[i]>=0)
+        take = 1 + solve(i , j-coins[i] ,coins,n);
+        return dp[i][j]=min(take,notake);
+    }
     int coinChange(vector<int>& coins, int amount) {
-        // wt array = coins array
-        // capacity = amount
-        // value array not given
-        // array size
         int n = coins.size();
-        int t[n+1][amount+1];
-        for(int i=0;i<n+1;i++){
-            t[i][0] = 0;
-        }
-        for(int j=0;j<amount+1 ;j++){
-            t[0][j] = INT_MAX -1 ;
-        }
-        for(int j =1 ;j<amount +1 ;j++){
-            if( j % coins[0] ==0){
-                t[1][j] = j / coins[0] ;
-            }
-            else{
-                t[1][j] = INT_MAX-1 ;
-            }
-        }
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<amount+1;j++){
-                if(coins[i-1] <= j){
-                    t[i][j] = min(t[i][j-coins[i-1]]+1 , t[i-1][j]) ;
-                }
-                else{
-                    t[i][j] = t[i-1][j] ;
-                }
-            }
-        }
-        if(t[n][amount]==INT_MAX-1) return -1;
-        return t[n][amount] ;
+        dp.resize(n+1,vector<int>(amount+1,-1));
+        int ans = solve(0,amount,coins,n);
+        if(ans>=1e9) return -1;
+        return solve(0,amount,coins,n);
     }
 };
