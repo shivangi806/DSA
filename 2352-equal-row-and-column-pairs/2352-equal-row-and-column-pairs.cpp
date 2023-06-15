cf66eba55a51e072386f5eb78773ee32ca573223
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-15 08:40:23 +0530

    Time: 1074 ms (5.01%), Space: 571.7 MB (5.08%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,38 +1,20 @@
 class Solution {
 public:
-    string mapping(vector<int> & row){
-
-        string ans="";
-
-        for(int j:row)
-            ans+=to_string(j)+"#";
-        
-        return ans;
-    }
-
     int equalPairs(vector<vector<int>>& grid) {
-
-        int count=0;
-        int n=grid.size();
-        unordered_map<string,int> mp;
-
-        // mapping the rows with the help of mapping function to create keys
-        for(int i=0; i<n;i++)
-            mp[mapping(grid[i])]++;
-        
-
-        for(int i=0; i<n;i++){
-
-            string ans="";
-
-            //Traversing the matrix column wise and generating new key 
-            for(int j=0; j<n;j++)
-                ans+=to_string(grid[j][i])+"#";
-
-            // adding the number of occurences of the key generated 
-            count+=mp[ans];
+        multiset<string> ms1, ms2;
+        for(int i=0;i<grid.size();i++){
+            string rows , cols;
+            for(int j=0;j<grid[0].size();j++){
+                rows = rows + " " + to_string(grid[i][j]);
+                cols=cols+" "+to_string(grid[j][i]);
+            }
+            ms1.insert(rows);
+            ms2.insert(cols);
         }
-
-        return count;
+        int c=0;
+        for(auto it : ms1){
+            c+=ms2.count(it);
+        }
+        return c;
     }
 };
