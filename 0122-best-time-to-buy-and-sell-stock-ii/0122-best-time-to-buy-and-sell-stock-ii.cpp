8c8d71b255e9eeb9c0be782ea2bea2eaddd77f23
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-27 14:06:09 +0530

    Time: 7 ms (74.71%), Space: 12.9 MB (99.81%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int n = p.size();
+        int ans =0;
+        for(int i=1;i<n;i++){
+            if(p[i]>p[i-1]){
+                ans+=(p[i]-p[i-1]);
+            }
+        }
+        return ans ;
+    }
+};
