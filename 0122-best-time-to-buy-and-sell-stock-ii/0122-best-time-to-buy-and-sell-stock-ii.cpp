d592121e61dc8eca75644def98a8832b0cbf8de1
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-27 16:20:48 +0530

    Time: 20 ms (8.51%), Space: 16.8 MB (18.85%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
+    
+    int dp[100005][3];
+    int solve(int i , int canbuy , vector<int>&p){
+        if(i==p.size()){
+            return 0;
+        }
+        
+        if(dp[i][canbuy]!=-1) return dp[i][canbuy];
+        int ans = 0 ;
+        if(canbuy==1){
+            int buykaro = -p[i] + solve(i+1 ,0, p);
+            int nibuykaro = solve(i+1,1,p);
+            
+            ans=max(buykaro,nibuykaro);
+        }
+        else{
+            int sellkaro = p[i]+solve(i+1,1,p);
+            int nisellkaro = solve(i+1,0,p);
+            ans = max(sellkaro,nisellkaro);
+        }
+        return dp[i][canbuy] = ans ;
+    }
     int maxProfit(vector<int>& p) {
         int n = p.size();
-        int ans =0;
-        for(int i=1;i<n;i++){
-            if(p[i]>p[i-1]){
-                ans+=(p[i]-p[i-1]);
-            }
-        }
-        return ans ;
+        memset(dp,-1,sizeof(dp));
+        return solve(0,1,p);
     }
 };
