4db001cc995746ff8127a6febd6333c84705870b
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-28 16:51:17 +0530

    Time: 2205 ms (17.12%), Space: 6.6 MB (73.59%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,52 +1,28 @@
 class Solution {
 public:
-    vector<long long int>dp;
-    bool isPalin(string &t){
-        int i = 0;
-        int j = t.size()-1;
-        while(i<j){
-            if(t[i]!=t[j]){
-                return false;
-            }
-            i++;
-            j--;
+    int palin(int i,int j,string &t){
+        while(j>i){
+            if(t[i]!=t[j])return 0;
+            i++;j--;
         }
-        return true;
+        return 1;
     }
-    int fun(int idx,int n,string &s){
-        if(idx==s.size()-1){
-            return 0;
+    int findans(string &s,vector<int>&dp,int i,int n){
+        if(i==n)return 0;
+        if(dp[i]!=-1)return dp[i];
+        int mini = 1e6;
+        for(int k = i;k<n;k++){
+             if(palin(i,k,s)){
+                  int cost = 1 + findans(s,dp,k+1,n);
+                  mini = min(mini,cost);
+             }
         }
-        if(dp[idx]!=-1){
-            return dp[idx];
-        }
-        long long maxi = 1e9;
-        string t = s.substr(idx);
-        if(isPalin(t)) return 0;
-        for(int i=idx;i<s.size();i++){
-            string t = s.substr(idx,i-idx+1);
-            if(isPalin(t)){
-                long long tmp =  1+fun(i+1,n,s);
-                maxi = min(maxi,tmp);
-            }
-        }
-        return dp[idx] = (int)maxi;
+        return dp[i] = mini;
     }
     int minCut(string s) {
-        if(isPalin(s)){
-            return 0;
-        }
         int n = s.size();
-        string tt = s.substr(0,n-1);
-        if(isPalin(tt) && isPalin(s)){
-            return 0;
-        }
-        if(isPalin(tt) && !isPalin(s)){
-            return 1;
-        }
-        
-        dp.resize(n+1,-1);
-        int ans = fun(0,n,s);
-        return ans;
+        vector<int>dp(n,-1);
+        if(palin(0,n-1,s))return 0;
+        return findans(s,dp,0,n)-1;
     }
 };
