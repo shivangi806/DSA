16ae976778d0fee28f4c6afdc7c39ba0df5430bb
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-28 15:29:19 +0530

    Time: 407 ms (42.58%), Space: 179.3 MB (9.07%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    
-    bool solve(int i , int n , vector<int>&nums,int tg, vector<vector<int>>&dp){
+    bool solve(int i , vector<int>&nums , int tg,vector<vector<int>>&dp){
         if(tg==0) return 1;
-        if(i==n-1){
-            if(tg==nums[i]) return 1;
-            return 0;
-        }
+        if(i>=nums.size()) return 0;
         
         if(dp[i][tg]!=-1) return dp[i][tg] ;
-        bool notake = solve(i+1,n,nums,tg,dp);
-        bool take = 0;
-        if(tg-nums[i]>=0) take = solve(i+1,n,nums,tg-nums[i],dp);
+        bool t =0;
+        if(tg-nums[i]>=0) t = solve(i+1,nums,tg-nums[i],dp);
+        bool nt = solve(i+1 , nums , tg,dp);
         
-        return dp[i][tg] = (take || notake) ;
+        return dp[i][tg] = t || nt ;
     }
-    bool canPartition(vector<int>& nums){
+    bool canPartition(vector<int>& nums) {
+        /// s1+s2 = s;
+        // s1=s/2;
+        // find subset whose sum is s/2
         int n = nums.size();
-        int tg=0;
-        for(int i=0;i<n;i++) tg+=nums[i];
-        if(tg%2!=0) return 0;
-        vector<vector<int>> dp(n+1,vector<int>(tg+1,-1));
-        return solve(0,n,nums,tg/2,dp);
+        int sum=0;
+        for(int i=0;i<n;i++) sum+=nums[i];
+        if(sum%2!=0) return 0;
+        vector<vector<int>> dp(n+1 , vector<int>(sum+1 , -1));
+        return solve(0,nums,sum/2,dp);
     }
 };
