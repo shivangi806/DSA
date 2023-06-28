ab1cd4137d482876a87fb707e0f15c55f2b2a70f
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-06-28 17:23:24 +0530

    Time: 2899 ms (5.01%), Space: 6.6 MB (73.59%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -8,9 +8,10 @@ public:
         return 1;
     }
     int solve(string &s ,int i,vector<int>&dp){
-        if(i>=n) return 0;
+        if(i>=n-1) return 0;
         if(dp[i]!=-1) return dp[i];
-        int ans = 1e6;
+        if(pal(s,i,n-1)) return 0;
+        int ans = INT_MAX ;
         for(int k=i;k<n;k++){
             if(pal(s,i,k)){
                 int temp = 1 + solve(s,k+1,dp);
@@ -21,9 +22,7 @@ public:
     }
     int minCut(string s) {
         n=s.size();
-        vector<int>dp;
-        dp.resize(n+1,-1);
-        if(pal(s,0,n-1)) return 0;
-        return solve(s,0,dp)-1; 
+        vector<int> dp(n,-1);
+        return solve(s,0,dp); 
     }
 };
