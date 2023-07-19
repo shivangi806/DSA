5b5248c7a05a35346f671b0f1be3d3e6a4b07383
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-07-19 17:21:03 +0530

    Time: 25 ms (10.77%), Space: 10.7 MB (12.77%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -4,7 +4,6 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
         
-        // if(i<0 || j<0 || i>=n || j>=m) return INT_MIN;
         if(i>=n) return 0;
         if(dp[i][j]!=INT_MIN) return dp[i][j];
         
@@ -13,8 +12,6 @@ public:
         int z = matrix[i][j] + solve(i+1,min(j+1,n-1),matrix,dp);
         
         return dp[i][j] = min({x,y,z});
-       
-       // return dp[i][j] = matrix[i][j] + min({solve(i+1, max(0, j-1), matrix,dp),solve(i+1, j, matrix, dp),solve(i+1, min(n-1, j+1), matrix, dp)});
     }
 
     int minFallingPathSum(vector<vector<int>>& mt) {
@@ -28,32 +25,3 @@ public:
         return ans ;
     }
 };
-// class Solution {
-// public:
-//     int minFallingPathSumUtil(int row, int col, vector<vector<int>>& matrix, int n, vector<vector<int>>& dp) {
-
-//     if(row >= n) return 0;
-
-//     if(dp[row][col] != INT_MIN) return dp[row][col];
-
-//     return (dp[row][col] = matrix[row][col] + min({
-//        minFallingPathSumUtil(row+1, max(0, col-1), matrix, n, dp),
-//        minFallingPathSumUtil(row+1, col, matrix, n, dp),
-//        minFallingPathSumUtil(row+1, min(n-1, col+1), matrix, n, dp)
-//     }));
-
-// }
-// int minFallingPathSum(vector<vector<int>>& matrix) {
-
-//     int n = matrix.size();
-
-//     vector<vector<int>> dp(n, vector<int>(n, INT_MIN));
-//         int mini = INT_MAX;
-//         for(int i = 0; i < n; i++) { 
-
-//             mini = min(mini, minFallingPathSumUtil(0, i, matrix, n, dp));
-//         }
-
-//         return mini;
-//     }
-// };
