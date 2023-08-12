a779ceac01fdc201cfdc18f43896653963dbcae6
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-08-13 02:10:19 +0530

    Added solution - LeetHub

---
+++ Minimum steps to destination - GFG/minimum-steps-to-destination.cpp
@@ -0,0 +1,42 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int minSteps(int target){
+        target = abs(target);
+        int step = 0;
+        int sum = 0;
+        while (sum < target) {
+            step++;
+            sum += step;
+        }
+        while ((sum - target) % 2 != 0) {
+            step++;
+            sum += step;
+        }
+        return step;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int D;
+        cin>>D;
+        
+        Solution ob;
+        cout<<ob.minSteps(D)<<"\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
