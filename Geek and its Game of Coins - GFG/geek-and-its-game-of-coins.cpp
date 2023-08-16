c89a7a37fa7656218f51e30ebe884f6ba2f73ece
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-08-16 14:26:43 +0530

    Added solution - LeetHub

--- Geek and its Game of Coins - GFG/geek-and-its-game-of-coins.cpp
+++ Geek and its Game of Coins - GFG/geek-and-its-game-of-coins.cpp
@@ -8,17 +8,16 @@ class Solution{
 	public:
 	
 	int solve(int n ,int x , int y,vector<int>&dp){
-	   // if(n==0) return 0;
 	    if(n==1) return 1;
 	    int a=1,b=1,c=1;
-	    
+	    bool ans = 0;
 	    if(dp[n]!=-1) return dp[n];
-	    if(n-x>=0) a = solve(n-x,x,y,dp);
+	    if(n-x>=0) a= solve(n-x,x,y,dp);
 	    if(n-y>=0) b = solve(n-y,x,y,dp);
 	    if(n-1>=0) c = solve(n-1,x,y,dp);
 	    
 	    if(!a | !b | !c) return dp[n]=1;
-	    return dp[n]=0;
+	   return dp[n] = 0 ;
 	}
 	int findWinner(int N, int X, int Y)
 	{
