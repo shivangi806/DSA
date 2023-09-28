ab2313d01a795f2cd609bc4f2e80a1544d5b7888
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-09-28 11:32:15 +0530

    Time: 8 ms (48.20%), Space: 16.6 MB (25.63%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -4,17 +4,17 @@ public:
         int n = nums.size();
         vector<int> ans(n);
         int k=0;
+        int l=n-1;
         for(int i=0;i<n;i++){
             if(nums[i]%2==0){
                 ans[k]=nums[i];
                 k++;
             }
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i]%2!=0){
-                ans[k]=nums[i];
-                k++;
+            else if(nums[i]%2!=0){
+                ans[l]=nums[i];
+                l--;
             }
+            
         }
         return ans ;
     }
