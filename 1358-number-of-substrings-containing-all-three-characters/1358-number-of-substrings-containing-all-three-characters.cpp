e5944ee6e9df0dd38fd84827aaba96141e48c660
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-09-30 13:10:07 +0530

    Time: 54 ms (53.33%), Space: 14.7 MB (9.90%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     
-    int numberOfSubstrings(string s) {
+    int atmost(string &s){
+        int n = s.size();
         int i=0;
         int j=0;
-        int n =s.size();
-        int ans=0 ;
-        map<char,int> m;
-        for(int j=0;j<n;j++){
-            m[s[j]]++;
-            while(m['a'] > 0 && m['b'] > 0 && m['c'] > 0 && i < j){
-                m[s[i]]--;
-                ans += (n-j);
-                i++;
+        unordered_map<char,int> mp;
+        int ans = 0;
+        while(i<n){
+            mp[s[i]]++;
+            
+            while(mp.size()==3){
+                mp[s[j]]--;
+                
+                if(mp[s[j]]==0) mp.erase(s[j]);
+                j++;
+                
+                
             }
+            
+            ans += (i-j+1);
+            i++;
         }
-        return ans ;
+        
+        return ans;
+    }
+    int numberOfSubstrings(string s) {
+        long long n = s.size();
+        long long totalSubarr = (n*(n+1))/2;
+        long long not_abc = atmost(s);
+        return totalSubarr - not_abc ;
     }
 };
