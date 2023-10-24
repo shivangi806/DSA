595804720e18ef044f752e75a1a24aef9aa7de16
shivangi806 <97212405+shivangi806@users.noreply.github.com>
2023-10-24 19:06:03 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (47.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,33 +1,49 @@
 class Solution {
 public:
-    void solve(int i,  vector<string> &ans,string &temp , string&digits,map<char,string>&m){
-        if(i==digits.size()){
-            ans.push_back(temp);
-            return ;
+//     void solve(int i, string &digits, string &ds, vector<string>& ans, map<char, string>& mp) {
+//         if (i == digits.size()) {
+//             ans.push_back(ds);
+//             return;
+//         }
+
+//         string tmp = mp[digits[i]];
+//         for (int k = 0; k < tmp.size(); k++) {
+//             ds.push_back(tmp[k]);
+//             solve(i + 1, digits, ds, ans, mp);
+//             ds.pop_back();
+//         }
+//     }
+    
+    void solve(int i , string &dg,string &ds, vector<string>&ans,map<char, string> &mp){
+        if(i==dg.size()){
+            ans.push_back(ds);
+            return;
         }
-        string str = m[digits[i]];
         
-        for(int j=0;j<str.size();j++){
-            temp.push_back(str[j]);
-            solve(i+1,ans,temp,digits,m);
-            temp.pop_back();
+        string tmp = mp[dg[i]];
+        for(int k=0;k<tmp.size();k++){
+            ds.push_back(tmp[k]);
+            solve(i+1,dg,ds,ans,mp);
+            ds.pop_back();
         }
+        
+        
     }
     vector<string> letterCombinations(string digits) {
-        map<char  , string> m;
-        m['2']="abc";
-        m['3']="def";
-        m['4']="ghi";
-        m['5']="jkl";
-        m['6']="mno";
-        m['7']="pqrs";
-        m['8']="tuv";
-        m['9']="wxyz";
-        
+        map<char, string> mp;
+        mp['2'] = "abc";
+        mp['3'] = "def";
+        mp['4'] = "ghi";
+        mp['5'] = "jkl";
+        mp['6'] = "mno";
+        mp['7'] = "pqrs";
+        mp['8'] = "tuv";
+        mp['9'] = "wxyz";
+
+        string ds;
         vector<string> ans;
         if(digits.size()==0) return ans;
-        string temp;
-        solve(0,ans,temp,digits,m);
+        solve(0, digits, ds, ans, mp);
         return ans;
     }
 };
